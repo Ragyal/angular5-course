08d5c8805320a2eb241e26a39ff091c453a4a70a
Ragyal <26764163+Ragyal@users.noreply.github.com>
2018-04-29 15:30:15 +0200

    Task 3 - Practicing Directives    *ngIf to (not) display a paragraph    *ngFor to list logged events    [ngStyle] to apply a backgroud-color using a function to get the specific color for an element    [ngClass] to apply a css-class if a function returns true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  isDisplayed: boolean = false;
+  loggedClicks: String[] = [];
+
+  onToggleDisplay() {
+    this.isDisplayed = !this.isDisplayed;
+    this.loggedClicks.push(Date.now().toString());
+  }
+
+  isGreater4(log) {
+    return 3 < this.loggedClicks.findIndex(l => log == l);
+  }
+
+  getColor(log) {
+    if (this.isGreater4(log)) {
+      return "blue"
+    } else {
+      return "white";
+    }
+  }
 }
